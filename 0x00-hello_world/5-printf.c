dccdff65c1ab6235b45882d6ea80482d43bc1f22
Musahammed <musahammedosiriame@gmail.com>
2022-07-01 10:55:16 -0700

    C Hello World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/
+* main - Entry point
+* Return: Always 0 (succes)
+*/
+
+intmain(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	reurn(0);
+}
