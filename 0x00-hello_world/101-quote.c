75461b9e07aebc1f0b5d83c4ff8f59b6acc88a87
Musahammed <musahammedosiriame@gmail.com>
2022-07-02 15:18:47 -0700

    C Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	Write("and that piece of art is useful\" - Dore Korpar, 2015 - 10 -19\n");
+	printf("and that piece of art is useful\" - Dore Korpar, 2015 - 10 -19\n");
 	return (1);
 }
