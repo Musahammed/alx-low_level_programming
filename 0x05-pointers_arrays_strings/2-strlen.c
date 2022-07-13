a3fd7bd7d0e46cf2c360d012364a620f6a92238b
Musahammed <musahammedosiriame@gmail.com>
2022-07-13 11:24:22 -0700

    task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,12 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strlen - lenght of string
  * @s: A pointer to an int that will be changed/update
  *
- * Return: void tha means our answer is correct
+ * Return: lenght of the string
  */
 int _strlen(char *s)
 {
-int len;
-
-while (*s != '\0')
-{
-len += 1;
-*s = *s + 1;
-}
-Return: (len);
+return (strlen(s));
 }
