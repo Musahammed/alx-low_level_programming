6fc1b3f658408fabdc3e0bea9e1e9180ae8c2247
Musahammed <musahammedosiriame@gmail.com>
2022-07-18 10:14:31 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,17 +2,17 @@
 
 /**
  * _memcpy - function of memory copy
- * @dest: 1st param
- * @n: 2nd param
- * @src: 3rd param
- * Return: memory area replace
+ * @dest: destination of memory
+ * @n: number of bytes to be copied
+ * @src: sourc of memory
+ * Return: string
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
-
+{
 unsigned int a;
 
 for (a = 0; a < n; a++)
-{
-dest[a] = src[a]
-}
+dest[a] = src[a];
+
 return (dest);
+}
