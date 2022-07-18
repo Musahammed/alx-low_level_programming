2cdab9b009fe92a09bebdc5c9d9225b862d371ef
Musahammed <musahammedosiriame@gmail.com>
2022-07-18 10:52:28 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,29 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strspn - function that get a lengths of substring
- * @s: 1st param
- * @accept: 2nd param
- * Rreturn: 
+ * @s:  source string 
+ * @accept: accept string
+ * Return: number of bytes in init segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int a = 0, b, t = 0;
+
+while (accept[a])
+{
+b = 0;
+
+while (s[b] != 32)
+{
+if (accept[a] == s[b])
+{
+t++;
+}
+b++;
+}
+a++;
+}
+return (t);
+}
 
