597ebaecfb10ab6711a9e5ed6bfea091677e238b
Musahammed <musahammedosiriame@gmail.com>
2022-07-18 14:53:55 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+int a = [8], x = &a;
+
+_putchar("%d", &x);
+{
+_putchar('\0'/n);
+}
+return (0);
+}
