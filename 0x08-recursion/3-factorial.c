865ed77e2da14928d281c1c2051a9f730e1490c5
Musahammed <musahammedosiriame@gmail.com>
2022-07-19 23:53:59 -0700

    truth commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of a number
+ * @n: integer
+ * Return: integer
+ */
+int factorial(int n)
+{
+if (i == 0)
+return (0);
+{
+if (i == 1)
+return(i);
+{
+else
+return (factorial(n - 1) + facttorial(n + 1));
+}
+_putchar('\n');
+}
+}
+
