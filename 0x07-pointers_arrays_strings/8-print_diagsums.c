28fbf3a7234ef53c8271e190a58bafc7cb9627b2
Musahammed <musahammedosiriame@gmail.com>
2022-07-20 03:24:18 -0700

    truth commit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,23 +1,19 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_diagsums
- * @a: pointer
- * @size: the size
- * Return: nothing
+ * print_chessboard - prints a chessboard
+ * @a: array of characters
  */
-void print_diagsums(int *a, int size)
+void print_chessboard(char (*a)[8])
 {
-int i, sum1 = 0, sum2 = 0;
+int i, j;
 
-for (i = 0; i < size; i++)
+for (i = 0; i < 8; i++)
 {
-sum1 += *(a + (size * i + i));
-
-sum2 += *(a + (size * i + size - 1 - i));
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
 }
-printf("%d, ", sum1);
-printf("%d\n", sum2);
+_putchar('\n');
 }
-
-
+}
