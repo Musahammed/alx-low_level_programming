a69d134c0aa894486d3cadd406ecd208a9b0ddc1
Musahammed <musahammedosiriame@gmail.com>
2022-07-24 01:40:42 -0700

    truth commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,23 +2,25 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two numbers.
- * @argc: argument count
- * @argv: arguments
+ * main - multiplies two numbers
+ * @argc: n args
+ * @argv: arr args
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int x, y;
+int i, val = 1;
 
-if (argc < 3)
+if (argc != 3)
 {
 printf("Error\n");
+
 return (1);
 }
-x = atoi(argv[1]);
-y = atoi(argv[2]);
-
-printf("%d\n", x * y);
+for (i = 1; i < argc; i++)
+{
+val *= atoi(argv[i]);
+}
+printf("%d\n", val);
 return (0);
 }
